6d87a45fc57a68afafa483a2efcb8683500f0453
IAN ESTEBAN TAUZY LAMAGNA <iantaulam@alu.edu.gva.es>
2022-10-28 11:33:37 +0200

    Terminado-Clase

--- MemeMaker/BorderConverter.cs
+++ MemeMaker/BorderConverter.cs
@@ -4,6 +4,8 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace MemeMaker
@@ -12,11 +14,14 @@ namespace MemeMaker
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            //String name = targetType.;
-            if(value is bool )
-            {
-
-            }
+            // Lo que me llega en value es un IsChecked, entonces es un boolean, pero aqui es un objeto
+            // Entonces no sabe lo que es, por eso hay que castearlo a bool
+            // Tambien podemos devoler solamente un numero. return 2 || return 0. Y se lo traga
+            bool res = (bool)value;
+            if (res)
+               return new Thickness(2);
+            else
+               return new Thickness(0);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- MemeMaker/MainWindow.xaml.cs
+++ MemeMaker/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace MemeMaker
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
